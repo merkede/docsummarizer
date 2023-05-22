da23bba85afc48d3114ef0628a980ed42e40f108
merkede <123862480+merkede@users.noreply.github.com>
2023-05-22 18:18:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     st.sidebar.markdown("""<small> This is open-source using GPT-4 under the hood. Expose your data at your own risk! .</small>""", unsafe_allow_html=True)
 
 
-    if st.button('Summarise (only click me once ... please)'):
+    if st.button('Summarise (only click me once ... for Gods sake)'):
         if input_method == 'Upload a document':
             process_summarize_button(uploaded_file, api_key, use_gpt_4, find_clusters)
 
